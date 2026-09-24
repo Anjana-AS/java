ef4f2255c9e424de7cb2ff42e7194271611f94e5
Anjana AS <144725021+Anjana-AS@users.noreply.github.com>
2026-09-24 13:34:31 +0530

    Add files via upload

--- client.java
+++ client.java
@@ -3,21 +3,27 @@ package anjana;
 import java.net.*;
 import java.io.*;
 import java.util.*;
-
 public class client {
     public static void main(String[] args) throws Exception {
         Socket s = new Socket("localhost", 6000);
-
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter message: ");
-        String msg = sc.nextLine();
-
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(s.getInputStream()));
         PrintWriter out = new PrintWriter(
             s.getOutputStream(), true);
-
-        out.println(msg);
-
+        while (true) {
+            System.out.print("Client: ");
+            String msg = sc.nextLine();
+            out.println(msg);
+            if (msg.equalsIgnoreCase("exit")) {
+                break;
+            }
+            String reply = in.readLine();
+            System.out.println("Server: " + reply);
+            if (reply.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
         s.close();
         sc.close();
     }
--- server.java
+++ server.java
@@ -2,22 +2,33 @@ package anjana;
 
 import java.net.*;
 import java.io.*;
-
+import java.util.*;
 public class server {
     public static void main(String[] args) throws Exception {
         ServerSocket ss = new ServerSocket(6000);
         System.out.println("Waiting for client...");
-
         Socket s = ss.accept();
-
+        System.out.println("Client connected!");
+        Scanner sc = new Scanner(System.in);
         BufferedReader in = new BufferedReader(
             new InputStreamReader(s.getInputStream()));
-
-        String msg = in.readLine();
-        System.out.println("Client says: " + msg);
-
+        PrintWriter out = new PrintWriter(
+            s.getOutputStream(), true);
+        while (true) {
+            String msg = in.readLine();
+            System.out.println("Client: " + msg);
+            if (msg.equalsIgnoreCase("exit")) {
+                break;
+            }
+            System.out.print("Server: ");
+            String reply = sc.nextLine();
+            out.println(reply);
+            if (reply.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
         s.close();
         ss.close();
+        sc.close();
     }
 }
-
